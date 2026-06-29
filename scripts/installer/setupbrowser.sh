4351a37504be36403540028a5eab80ca06c87794
cfels <moxiix@proton.me>
2026-06-29 13:14:16 +0200

    fix typo

--- scripts/installer/setupbrowser.sh
+++ scripts/installer/setupbrowser.sh
@@ -4,4 +4,4 @@ set -e
 
 echo "copy profile"
 cp -r $HOME/nixos-dots/Configs/lb_profile/profile/* $HOME/.config/librewolf/librewolf/*.default/
-echo "done! (this soltution maybe works)"
+echo "done! (this solution maybe works)"
