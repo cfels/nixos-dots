2f0d4f34ba023854e39a4dbdd7b88e189e909f1a
cfels <moxiix@proton.me>
2026-07-28 13:14:27 +0200

    add fish alias

--- Configs/config/.config/fish/config.fish
+++ Configs/config/.config/fish/config.fish
@@ -18,5 +18,5 @@ alias vac="vac-status"
 alias vps="ssh root@104.245.245.82"
 alias server="ssh -p 2222 root@192.168.18.138"
 alias rm='rm -i'
-alias rm -rf='rm -rf -i'
 alias sudo='doas'
+alias conf='nvim $HOME/nixos-dots/Configs/config/.config'
