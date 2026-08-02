030e37eca8ee9c7dac406d04ee0c96bbdb0252fc
cfels <moxiix@proton.me>
2026-08-02 17:15:59 +0200

    sadasda

--- Configs/nixos/home/default.nix
+++ Configs/nixos/home/default.nix
@@ -10,15 +10,6 @@
     username = "moxiu";
     homeDirectory = "/home/moxiu";
     stateVersion = "26.05";
-    
-    pointerCursor = {
-      enable = true;
-      name = "Bibata-Modern-Ice";
-      size = 24;
-      package = lib.mkForce pkgs.bibata-cursors;
-      gtk.enable = true;
-      x11.enable = true;
-    };
   };
 
   programs.home-manager.enable = true;
